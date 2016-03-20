cfe544e91cbc91cb9781c9c786034b7f7e0de4ef
Michael Robinson <michael.robinson@solnet.co.nz>
2016-03-21 09:56:35 +1300

    Updated plugin init for iOS to work with Cordova >= 4.0. Fixes #40

--- src/ios/StreamingMedia.m
+++ src/ios/StreamingMedia.m
@@ -25,13 +25,6 @@ NSString * const TYPE_VIDEO = @"VIDEO";
 NSString * const TYPE_AUDIO = @"AUDIO";
 NSString * const DEFAULT_IMAGE_SCALE = @"center";
 
-- (CDVPlugin*) initWithWebView:(UIWebView*)theWebView {
-	NSLog(@"-------------------------------------------------");
-	NSLog(@"INITWITHWEBVIEW");
-	self = (StreamingMedia*)[super initWithWebView:theWebView];
-	return self;
-}
-
 -(void)parseOptions:(NSDictionary *)options type:(NSString *) type {
 	// Common options
 	if (![options isKindOfClass:[NSNull class]] && [options objectForKey:@"shouldAutoClose"]) {
