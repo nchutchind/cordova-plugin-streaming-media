98707c323b1a72c3f529498365092c8221dd3ed2
milad kian <milad.kian@local.com>
2016-07-23 14:11:26 +0430

    fix mMediaController show event

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleAudioStream.java
@@ -277,7 +277,12 @@ public class SimpleAudioStream extends Activity implements
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		mMediaController.show();
+		
+		if(mMediaController.isShowing()){
+			mMediaController.hide();
+		}else{
+			mMediaController.show();
+		}
 		return false;
 	}
 }
