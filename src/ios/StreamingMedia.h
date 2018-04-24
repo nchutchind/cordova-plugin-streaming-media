2cb0e837e2376f4d20b19db8990e942eae34e45e
Tian Qisheng <CrispyFriedPig@users.noreply.github.com>
2018-04-24 12:41:29 +0800

    Ignore the mute button on IOS
    Ignore the mute button when playing a video on IOS.

--- src/ios/StreamingMedia.h
+++ src/ios/StreamingMedia.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 #import <Cordova/CDVPlugin.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface StreamingMedia : CDVPlugin
+@property (nonatomic, strong) AVAudioSession* avSession;
 
 - (void)playVideo:(CDVInvokedUrlCommand*)command;
 - (void)playAudio:(CDVInvokedUrlCommand*)command;
